7501ff0adf15ce8b25663f015b93a1f4d5d328f7
AyushBansal88 <125378315+AyushBansal88@users.noreply.github.com>
2024-11-09 13:30:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// ================================================
+// script for header scroll Animation
+// ================================================
+function header_animate() {
+  if (window.scrollY > 150) {
+    header.classList.add('scroll_header');
+  } else {
+    header.classList.remove('scroll_header');
+  }
+}
+var header = document.getElementById('header');
+window.addEventListener("scroll", header_animate);
+window.addEventListener("load", header_animate);
+
+// script for animation
+AOS.init();
+
+
+
